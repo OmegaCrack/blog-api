c1727800265a17ca39d89634f11d0d91488331f8
Charles <charlesomukaya3@gmail.com>
2025-07-02 19:54:40 +0300

    Update on Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\Hasfactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
